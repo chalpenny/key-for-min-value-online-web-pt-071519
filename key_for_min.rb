7ccf4bfa2fdf3fdd337e6ec1574658cccd639cf1
Colleen Halpenny <cp.halpenny@gmail.com>
2019-07-27 22:37:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
-  name_hash.do |key_name, value_number|
-  if value_number < nil
+  name_hash.each do |key_name, value_number|
+  if value_number < nil || value_number < min_value]
       #replace min_value with value_number
       #how to return the key associated with min_value
 
